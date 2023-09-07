1dc7c19f488825ad9c51aaf48df1f408b28d3af7
Fredy Camas <fredycamasdev@gmail.com>
2023-09-07 14:28:37 -0400

    Container With Most Water

---
+++ LeeCode/Container With Most Water.playground/Contents.swift
@@ -0,0 +1,43 @@
+/*
+ 
+ Container With Most Water
+
+ You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+ Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+ Return the maximum amount of water a container can store.
+
+ Notice that you may not slant the container.
+ */
+
+// time complexity O(n)
+// space complexity O(1)
+
+class Solution {
+    func maxArea(_ height: [Int]) -> Int {
+        
+
+        var left = 0
+        var right = height.count - 1
+        var maxArea = 0
+
+
+        while left < right {
+
+            var portionArea = (right - left) * min(height[left], height[right])
+
+            maxArea = max(maxArea, portionArea)
+
+            if height[left] <= height[right]{
+                left += 1
+            }else {
+                right -= 1
+            }
+
+        }
+
+        
+        return maxArea
+    }
+}
