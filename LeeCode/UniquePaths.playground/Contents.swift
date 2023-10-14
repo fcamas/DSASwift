fbff69640a076b9eb7cfd6d69ef79a60ada375b0
Fredy Camas <fredycamasdev@gmail.com>
2023-10-13 22:24:10 -0400

    Unique Paths

---
+++ LeeCode/UniquePaths.playground/Contents.swift
@@ -0,0 +1,30 @@
+/*Unique Paths
+ 
+ There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+ 
+ Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+ 
+ The test cases are generated so that the answer will be less than or equal to 2 * 109.
+ */
+
+
+class Solution {
+    func uniquePaths(_ m: Int, _ n: Int) -> Int {
+        var dp = [[Int]](repeating: [Int](repeating: 0, count: n), count: m)
+        
+        for i in 0..<m {
+            dp[i][0] = 1
+        }
+        for j in 0..<n {
+            dp[0][j] = 1
+        }
+        
+        for i in 1..<m {
+            for j in 1..<n {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+            }
+        }
+        
+        return dp[m - 1][n - 1]
+    }
+}
