4c4df5530552c4a819a76bfeb31e6f4246220af6
Fredy Camas <fredycamasdev@gmail.com>
2023-10-17 00:42:51 -0400

    Coin Change

--- LeeCode/CoinChange.playground/Contents.swift
+++ LeeCode/CoinChange.playground/Contents.swift
@@ -7,19 +7,31 @@
  You may assume that you have an infinite number of each kind of coin.
  */
 
-class Solution {
-    func coinChange(_ coins: [Int], _ amount: Int) -> Int {
-    var dp = [Int](repeating: amount + 1, count: amount + 1)
-    dp[0] = 0
+    class Solution {
+        func coinChange(_ coins: [Int], _ amount: Int) -> Int {
+            //coins = integer of coins of different nomination
+            // ammount = total amount of money
+            //return  = fewest number of coin  to make up that amount
+            //return -1  = money can not be made up to amount( combination does exist)
 
-    for i in 1...amount {
-        for coin in coins {
-            if i >= coin {
-              
-                dp[i] = min(dp[i], dp[i - coin] + 1)
+            // a 2d container that holds all coins posibiliteis
+            // interate over amount
+            // for each amount iterate over each coin
+            // calculate min value of coin
+
+            guard amount > 0 else {
+                return 0
             }
+            var arr = Array(repeating: amount + 1, count: amount + 1)
+            arr[0] = 0
+
+            for i in 1...amount {
+                for coin in coins {
+                    if i >= coin{
+                    arr[i] = min(arr[i], arr[i - coin] + 1)
+                    }
+                }
+            }
+            return arr[amount] != amount + 1 ?  arr[amount] : -1
         }
     }
-    return dp[amount] > amount ? -1 : dp[amount]
-}
-}
