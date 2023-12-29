2913410a1b23e9ab1a1b2db9f202dce2e4b230a0
Fredy Camas <fredycamasdev@gmail.com>
2023-12-29 00:05:27 -0500

    added time complexity notes

--- LeeCode/Merge Two Sorted List.playground/Contents.swift
+++ LeeCode/Merge Two Sorted List.playground/Contents.swift
@@ -6,6 +6,9 @@
 
  Return the head of the merged linked list.*/
 
+//Time complexity: O(m + n)
+//Space complexity: O(1)
+
 class ListNode {
     var val: Int
     var next: ListNode?
@@ -16,50 +19,33 @@ class ListNode {
     }
 }
 
-func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
-    // Dummy node to simplify code
-    let dummy = ListNode(0)
-    var current: ListNode? = dummy
-    var l1 = list1
-    var l2 = list2
-    
-    while l1 != nil && l2 != nil {
-        if l1!.val < l2!.val {
+class Solution {
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        // Dummy node to simplify code
+        let dummy = ListNode(0)
+        var current: ListNode? = dummy
+        var l1 = list1
+        var l2 = list2
+        
+        while l1 != nil && l2 != nil {
+            if l1!.val < l2!.val {
+                current?.next = l1
+                l1 = l1?.next
+            } else {
+                current?.next = l2
+                l2 = l2?.next
+            }
+            current = current?.next
+        }
+        
+        // If one list is longer than the other
+        if l1 != nil {
             current?.next = l1
-            l1 = l1?.next
         } else {
             current?.next = l2
-            l2 = l2?.next
         }
-        current = current?.next
+        
+        return dummy.next
     }
     
-    // If one list is longer than the other
-    if l1 != nil {
-        current?.next = l1
-    } else {
-        current?.next = l2
-    }
-    
-    return dummy.next
 }
-
-// Example usage:
-// Assume list1 and list2 are already sorted linked lists
-let list1 = ListNode(1)
-list1.next = ListNode(3)
-list1.next?.next = ListNode(5)
-
-let list2 = ListNode(2)
-list2.next = ListNode(4)
-list2.next?.next = ListNode(6)
-
-let mergedList = mergeTwoLists(list1, list2)
-
-// Print the merged list values
-var current: ListNode? = mergedList
-while current != nil {
-    print(current!.val)
-    current = current?.next
-}
-
