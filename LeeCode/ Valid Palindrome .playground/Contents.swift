186cc289209e6405ff4bdb0cd89f0540f51c2e90
Fredy Camas <fredycamasdev@gmail.com>
2024-03-03 13:34:00 -0500

    added second solution

--- LeeCode/ Valid Palindrome .playground/Contents.swift
+++ LeeCode/ Valid Palindrome .playground/Contents.swift
@@ -1,6 +1,5 @@
 /*
  Valid Palindrome
- 
  A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
  
  Given a string s, return true if it is a palindrome, or false otherwise.
@@ -9,30 +8,6 @@
 
 //Time Complexity O(n)
 //Space ComplexityO(n)
-
-class Solution {
-    func isPalindrome(_ s: String) -> Bool {
-        let newArray = Array(s.lowercased())
-        var left = 0
-        var right = s.count - 1
-        
-        while left < right {
-            while left < right && !newArray[left].isLetter && !newArray[left].isNumber {
-                left += 1
-            }
-            while left < right && !newArray[right].isLetter && !newArray[right].isNumber {
-                right -= 1
-            }
-            if newArray[left] != newArray[right] {
-                return false
-            }
-            left += 1
-            right -= 1
-        }
-        
-        return true
-    }
-}
 
 class Solution2 {
     func isPalindrome(_ s: String) -> Bool {
