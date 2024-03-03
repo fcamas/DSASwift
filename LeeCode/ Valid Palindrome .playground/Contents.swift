0fdc32b241c963e555378e8fe7f390b030bf9cd4
Fredy Camas <fredycamasdev@gmail.com>
2024-03-03 14:44:00 -0500

    add new solution

--- LeeCode/ Valid Palindrome .playground/Contents.swift
+++ LeeCode/ Valid Palindrome .playground/Contents.swift
@@ -8,6 +8,29 @@
 
 //Time Complexity O(n)
 //Space ComplexityO(n)
+class Solution {
+    func isPalindrome(_ s: String) -> Bool {
+        let newArray = Array(s.lowercased())
+        var left = 0
+        var right = s.count - 1
+        
+        while left < right {
+            while left < right && !newArray[left].isLetter && !newArray[left].isNumber {
+                left += 1
+            }
+            while left < right && !newArray[right].isLetter && !newArray[right].isNumber {
+                right -= 1
+            }
+            if newArray[left] != newArray[right] {
+                return false
+            }
+            left += 1
+            right -= 1
+        }
+        
+        return true
+    }
+}
 
 class Solution2 {
     func isPalindrome(_ s: String) -> Bool {
