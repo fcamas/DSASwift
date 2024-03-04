369d1d361bf097c445e91a6da685d2c224a98371
Fredy Camas <fredycamasdev@gmail.com>
2024-03-04 17:45:00 -0500

    add solution

--- LeeCode/BackSpace String Compare .playground/Contents.swift
+++ LeeCode/BackSpace String Compare .playground/Contents.swift
@@ -11,6 +11,25 @@ import UIKit
 //Time Complexity: O(m + n) - where m and n are the lengths of strings s and t, respectively.
 //Space Complexity: O(m + n) - space is used to store processed strings.
 
+class Solution {
+    func backspaceCompare(_ s: String, _ t: String) -> Bool {
+        return processString(s) == processString(t)
+    }
 
+    func processString(_ str: String) -> String {
+        var result = [Character]()
 
+        for char in str {
+            if char == "#" {
+                if !result.isEmpty {
+                    result.removeLast()
+                }
+            } else {
+                result.append(char)
+            }
+        }
+
+        return String(result)
+    }
+}
 
